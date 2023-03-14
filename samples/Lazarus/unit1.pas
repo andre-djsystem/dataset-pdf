ab095b71f848c98e732c14523f88cec71cae554d
andre-djsystem <andre@djsystem.com.br>
2023-03-14 08:06:40 -0300

    Improve sample

--- samples/Lazarus/unit1.pas
+++ samples/Lazarus/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, memds, DB, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls, DBGrids;
+  ExtCtrls, DBGrids, libjpfpdf, libjpfpdfextends;
 
 type
 
@@ -15,6 +15,8 @@ type
   TForm1 = class(TForm)
     btAdd: TButton;
     btPDFFile: TButton;
+    cbDefaultHeader: TCheckBox;
+    cbDefaultFooter: TCheckBox;
     DBGrid1: TDBGrid;
     dsUsers: TDataSource;
     edName: TEdit;
@@ -28,7 +30,8 @@ type
     procedure btAddClick(Sender: TObject);
     procedure btPDFFileClick(Sender: TObject);
   private
-
+    procedure CustomHeader(APDF: TJPFpdfExtends);
+    procedure CustomFooter(APDF: TJPFpdfExtends);
   public
 
   end;
@@ -64,7 +67,37 @@ end;
 
 procedure TForm1.btPDFFileClick(Sender: TObject);
 begin
+  TDataSetPDFConfig.GetInstance.Title := 'Lista de Usuários';
+
+  TDataSetPDFConfig.GetInstance.PrintDefaultHeader := cbDefaultHeader.Checked;
+  if TDataSetPDFConfig.GetInstance.PrintDefaultHeader then
+    TDataSetPDFConfig.GetInstance.OnDoHeader := nil
+  else
+    TDataSetPDFConfig.GetInstance.OnDoHeader := @CustomHeader;
+
+  TDataSetPDFConfig.GetInstance.PrintDefaultFooter := cbDefaultHeader.Checked;
+  TDataSetPDFConfig.GetInstance.PrintDefaultFooter := cbDefaultHeader.Checked;
+  if TDataSetPDFConfig.GetInstance.PrintDefaultFooter then
+    TDataSetPDFConfig.GetInstance.OnDoFooter := nil
+  else
+    TDataSetPDFConfig.GetInstance.OnDoFooter := @CustomFooter;
   mtUsers.ToPDFFile('file.pdf','user.str');
+end;
+
+procedure TForm1.CustomHeader(APDF: TJPFpdfExtends);
+begin
+  APDF.SetUTF8(True);
+  APDF.SetFont(ffHelvetica, fsBold, 10);
+  APDF.Cell(0,20,'Custom Header','0',0,'C');
+  APDF.Ln(20);
+end;
+
+procedure TForm1.CustomFooter(APDF: TJPFpdfExtends);
+begin
+  APDF.SetUTF8(True);
+  APDF.SetY(-12);
+  APDF.SetFont(ffHelvetica,fsNormal,8);
+  APDF.Cell(0,10,'Custom Footer','0',0,'C');
 end;
 
 end.
